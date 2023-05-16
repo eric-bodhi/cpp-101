a73af857a92a57a52189aedc54b5f1ccb9cd0f46
Bodhi2011 <101071809+Bodhi2011@users.noreply.github.com>
2023-05-16 11:44:28 -0400

    Update sudoku.hpp

--- sudoku/sudoku.hpp
+++ sudoku/sudoku.hpp
@@ -3,10 +3,11 @@
 
 #include <vector>
 #include "grid.hpp"
+#include <optional>
 
 std::vector<int> shuffle(const std::vector<int>& v); 
 int pattern(int r, int c, int base);
-
+  std::vector<int> generateSequence(std::vector<int> const &base);
 
 class Sudoku {
 public:
@@ -15,17 +16,17 @@ public:
     void make_playable(int num_blanks);
     bool solve();
     bool is_valid() const;
-    std::vector<std::vector<int>> getBoard() const;
+    grid getBoard() const;
 
+private:
     bool validRows() const;
     bool validCols() const;
     bool validSquares() const;
     bool validUnit(const std::vector<int>& unit) const;
 
     bool validMove(int val, int row, int col) const;
-    std::pair<int, int> findEmptyCell() const;
+    std::optional<std::pair<int, int>> findEmptyCell() const;
 
-private:
     grid board;
 };
 
